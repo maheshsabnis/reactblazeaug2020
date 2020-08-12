7c468dcbfa40cb43ba7e6d68e2fb87205ec51d81
Mahesh Sabnis <sabnis_m@hotmail.com>
2020-08-12 17:46:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,12 +5,25 @@ import ReactDom from "react-dom";
 // 3. Import bootstrap
 import "!style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css";
 
-// FirstComponent, will be read as 'WebComponent' means that the
-// custom HTML DOM element
-import FirstComponent from './app/components/firstComponent';
-import ProductComponent from './app/components/productComponent';
+// import ans use the React-Redux object model for the application 
 
-let message = 'Hay React Hello!!!!';
+// 1. import createStore object from redux
+import { createStore } from 'redux';
+// 2. import reducer,  this will monitor the store updares
+import reducer from './app/reduxapp/reducers/reducer';
+// 3. import Provider from react-redux taht will
+// provide a singleton instance to the ReactCOmponent so that the componene will
+// use the store
+import { Provider } from 'react-redux';
 
-// mount the Component in the HTML element with 'id' as 'app'
-ReactDom.render( < ProductComponent / > , document.getElementById("app"));
+// define a store object using the createStore() menthod and pass reducer to it
+
+let store = createStore(reducer,
+     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+import ReduxMainComponent from './app/reduxapp/ReduxComponent';
+// render the React Component inside the Provider that is using the store object
+
+ReactDom.render(<Provider store={store}>
+       <ReduxMainComponent/>
+    </Provider>   , document.getElementById("app"));
